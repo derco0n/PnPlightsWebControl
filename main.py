a63fbaf3dc8b7644c8106a94f9fd4518ac5e10c3
derco0n <dennis_marx@web.de>
2020-12-12 15:40:21 +0100

    some changes

--- main.py
+++ main.py
@@ -10,18 +10,23 @@ class MyServer(BaseHTTPRequestHandler):
         with open(filename, 'r') as myFile:
             return myFile.read()
 
+    def tooglePlayersLight(self, on, id):
+        """ This will toggle a players light (determined by id) On (if true) or off"""
+        return
 
     def do_GET(self):
-        if self.path != "/":  # If not root path, deny access
+        if self.path == "/":  # If root path
+            # Request OK
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            content = self.getTextFromFile("./main.html")
+            self.wfile.write(bytes(content, "utf-8"))
+            return
+        else:
             self.send_response(403)
             self.wfile.write(bytes("403: You don't have access here. Try accessing '/' instead.", "utf-8"))
             return
-        # Request OK
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        content = self.getTextFromFile("./main.html")
-        self.wfile.write(bytes(content, "utf-8"))
 
 
 if __name__ == "__main__":        
