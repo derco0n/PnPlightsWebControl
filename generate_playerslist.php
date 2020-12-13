fa8c513dbe5a145c760ed6e5b090c50e3e55a6a6
derco0n <dennis_marx@web.de>
2020-12-13 19:22:11 +0100

    Improved UI

---
+++ generate_playerslist.php
@@ -0,0 +1,42 @@
+<?php 
+
+//Used to create the initial player-layout
+
+$num_players=8;
+
+echo('
+<div id="players">');
+for ($i=1; $i<$num_players+1; $i++){ 
+    echo('
+    <div id="p' . $i . '" class="block">
+        <div id="p' . $i . '_rank">
+            ' . $i . ': 
+        </div>
+        <div id="p' . $i . '_name">            
+            <input type="text" placeholder="Spielername" width="150" name="name_p' . $i . '" id="name_p' . $i . '" 
+            onchange="PlayerNameChanged(name_p' . $i . ')" onmouseenter="PlayerOnHover(\'p' . $i . '\')" onmouseleave="PlayerOnNoHover(\'p' . $i . '\')" />            
+        </div>
+        <div id="p' . $i . '_points">
+            <input type="number" min="0" placeholder="Punkte" width="30" name="points_p' . $i . '" id="points_p' . $i . '" onchange="PlayerPointsChanged(points_p' . $i . ')"/>
+        </div>
+    </div>'
+    );
+}
+echo('
+</div>');
+
+echo('
+
+########################
+');
+
+echo('
+    var player_uis = [];');
+for ($i=1; $i<$num_players+1; $i++){ 
+    echo('
+    player_uis.push({id: "p' . $i . '", rank: "p' . $i . '_rank", name: "p' . $i . '_name", textbox_name: "name_p' . $i . '", textbox_points: "points_p' . $i . '"});');
+    }
+    echo('
+
+    ');
+?>
